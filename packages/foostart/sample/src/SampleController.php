e8577e437dea1252aca8f5bacd481a2401596148
Sontunghunter <sontung.buinguyen@gmail.com>
2017-02-27 15:00:33 +0700

    First Commit

---
+++ packages/foostart/sample/src/SampleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Foostart\Sample;
+
+use App\Http\Controllers\Controller;
+
+Class SampleController extends Controller
+{
+
+    public function index()
+    {
+    	echo "this is my package!!!!!!!";
+    }
+
+}
---
+++ packages/foostart/sample/src/routes.php
@@ -0,0 +1,3 @@
+<?php 
+	Route::get('sample','Foostart\Sample\SampleController@index');
+?>
